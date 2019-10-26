83aa88edc78670a72d4b03e00f99b8b921437cd4
merry-attribute-0729 <adamdsmyth@gmail.com>
2019-10-26 21:04:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,3 +6,11 @@ end
 array=array.unshift (string)
 return array 
 end
+def using_pop(array,string)
+array=array.pop (string)
+return array 
+end
+def using_push(array,string)
+array=array.push (string)
+return array 
+end
